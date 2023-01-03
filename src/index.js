65df0813490d58875ef4eed94be075990b18065d
Shraddhanand <1shraddhanand@gmail.com>
2023-01-03 21:16:13 +0530

    connect link improved

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ const router = require("./Routes/router")
 const app = express()
 app.use(bodyParser.json())
 app.use(cors())
-app.use("/",router)
+app.use("/", router)
 
-mongoose.connect("mongodb+srv://spacespider:admin@cluster0.0ps1ymn.mongodb.net/product-management-assignment").then(()=>{
-    console.log("Mongodb connected...")
-    app.listen(4000,()=>{
-        console.log("Express running on", 4000)
+mongoose
+    .connect("mongodb+srv://functionup-radon-cohort:radon123@cluster0.zbsotuc.mongodb.net/group17Database?retryWrites=true&w=majority")
+    .then(() => {
+        console.log("Mongodb connected")
+        app.listen(6000, () => {
+            console.log("Express running on", 6000)
+        })
     })
-})
