f26e349a220a360a38c0d6d2347cc8e191875b90
Shraddhanand <1shraddhanand@gmail.com>
2023-01-04 16:04:24 +0530

    include the link of mongodb

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-const express=require('express')
-const mongoose=require('mongoose')
-const bodyparser=require('body-parser')
-const route=require('./routes/route')
-const app=express()
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyparser = require('body-parser')
+const route = require('./routes/route')
+const app = express()
 
 app.use(bodyparser.json())
 
 
 mongoose.set('strictQuery', true)
-mongoose.connect("mongodb+srv://musharrafansari:XY5t9CKinqT75evR@cluster0.xsylin5.mongodb.net/OrderDatabase", {
-    useNewUrlParser: true
-})
-.then(()=>console.log("mongoDB is connected"))
-.catch((error)=>console.log(error))
+mongoose
+    .connect("mongodb+srv://rzyHkFvcKuKG7Vde:rzyHkFvcKuKG7Vde@sahilpaylacluster.h8dndkb.mongodb.net/?retryWrites=false&w=majority")
+    .then(() => console.log("mongoDB is connected"))
+    .catch((error) => console.log(error))
 
 
-app.use('/',route)
+app.use('/', route)
 
-app.listen(4000,function(){
-    console.log("express app is running on PORT " + (4000))
+app.listen(6000, function () {
+    console.log("express app is running on PORT " + (6000))
 })
